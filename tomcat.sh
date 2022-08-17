1757f23b776748068f02cf5297137c8a6844a097
HoloEye <103514768+HoloEye@users.noreply.github.com>
2022-08-17 14:38:31 +0200

    tomcat configuration

--- tomcat.sh
+++ tomcat.sh
@@ -8,3 +8,52 @@ TOMDIR=`echo $EXTOUT | cut -d '/' -f1`
 useradd --shell /sbin/nologin tomcat
 rsync -avzh /tmp/$TOMDIR/ /usr/local/tomcat8/
 chown -R tomcat.tomcat /usr/local/tomcat8
+
+rm -rf /etc/systemd/system/tomcat.service
+
+cat <<EOT>> /etc/systemd/system/tomcat.service
+[Unit]
+Description=Tomcat
+After=network.target
+
+[Service]
+
+User=tomcat
+Group=tomcat
+
+WorkingDirectory=/usr/local/tomcat8
+
+#Environment=JRE_HOME=/usr/lib/jvm/jre
+Environment=JAVA_HOME=/usr/lib/jvm/jre
+
+Environment=CATALINA_PID=/var/tomcat/%i/run/tomcat.pid
+Environment=CATALINA_HOME=/usr/local/tomcat8
+Environment=CATALINE_BASE=/usr/local/tomcat8
+
+ExecStart=/usr/local/tomcat8/bin/catalina.sh run
+ExecStop=/usr/local/tomcat8/bin/shutdown.sh
+
+
+RestartSec=10
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+
+EOT
+
+systemctl daemon-reload
+systemctl start tomcat
+systemctl enable tomcat
+
+git clone -b local-setup https://github.com/devopshydclub/vprofile-project.git
+cd vprofile-project
+mvn install
+systemctl stop tomcat
+sleep 60
+rm -rf /usr/local/tomcat8/webapps/ROOT*
+cp target/vprofile-v2.war /usr/local/tomcat8/webapps/ROOT.war
+systemctl start tomcat
+sleep 120
+cp /vagrant/application.properties /usr/local/tomcat8/webapps/ROOT/WEB-INF/classes/application.properties
+systemctl restart tomcat
