878cb6e990da85a157673e7837e45aaa0e2a34a3
HoloEye <103514768+HoloEye@users.noreply.github.com>
2022-08-17 12:32:26 +0200

    installing database software

---
+++ mysql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DATABASE_PASS='admin123'
+sudo yum update -y
+sudo yum install epel-release -y
+sudo yum install git zip unzip -y
+sudo yum install mariadb-server -y
+
+
+# starting & enabling mariadb-server
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
